7c7dbbe3154c01eae1ec28560be917cc4de13af5
weqe <paul1tw1@gmail.com>
2024-04-06 13:45:02 +0300

    - create a sample vrrp json configuration file that can be used.

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 cargo build 
-sudo ./target/debug/failover --action setup
-sudo ./target/debug/failover 
+sudo ./target/debug/failover --file sample-vrrp-config.json --action setup
+sudo ./target/debug/failover --file sample-vrrp-config.json
 pid=$!
 wait $pid
-sudo ./target/debug/failover --action teardown
+sudo ./target/debug/failover --file sample-vrrp-config.json --action teardown
 
